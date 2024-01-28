ca02eb0aca48000b96291f9a01130b6fe1531e88
mariemzayn18 <mariamzayn18@gmail.com>
2024-01-28 10:41:47 +0200

    Basic configuration

---
+++ src/main/java/com/example/IMDbAPI/ImDbApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.IMDbAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImDbApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImDbApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/IMDbAPI/ImDbApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.IMDbAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImDbApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
