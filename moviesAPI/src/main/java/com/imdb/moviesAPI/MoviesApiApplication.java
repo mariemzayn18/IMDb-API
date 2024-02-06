8394d373266a09897b1648073cf9dd3b8f96554b
mariemzayn18 <mariamzayn18@gmail.com>
2024-02-06 20:09:41 +0200

    Removed useless scanning

--- moviesAPI/src/main/java/com/imdb/moviesAPI/MoviesApiApplication.java
+++ moviesAPI/src/main/java/com/imdb/moviesAPI/MoviesApiApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication(scanBasePackages = "com.imdb")
+@SpringBootApplication
 @EnableFeignClients
 public class MoviesApiApplication {
 
